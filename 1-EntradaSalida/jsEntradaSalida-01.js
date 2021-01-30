870557fd111d8d123506988e14e2cdb600fab582
lfritz1984 <leonelfritz@gmail.com>
2021-01-30 17:16:00 -0300

    2 alertas

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -4,5 +4,5 @@ se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
 	alert("Esto funciona de Maravilla");
+	alert("Esto funciona de Maravilla 2");
 }
-
