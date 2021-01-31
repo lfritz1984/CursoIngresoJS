2cd3f836111eb3397139b6587cf07fa65eb9d165
lfritz1984 <leonelfritz@gmail.com>
2021-01-31 13:00:07 -0300

    Ejercicio 2 terminado

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -4,7 +4,16 @@ y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	let nombre; // declaro la variable nombre
-	nombre="Jose"; // la inicializo con la cadena Jose
+	nombre = "Leonel"; // la inicializo con la cadena Jose
 	alert(nombre);
+
+    let apellido // aca declaro una nueva variable
+	apellido = prompt("Ingrese su Apellido"); // inicializo mediante un promt una cadena de texto
+	alert(apellido); //ejercicio de con comando prompt
+
+	/*const nombre = "Jose"; una constante es una variable de solo lectura = significa que en el unico lugar 
+	que puedo asignarle un valor es en el momento de la declaración y nunca mas puedo modificar el valor en la
+	constante nombre*/
+	//nombre = "Ana";
 }
 
