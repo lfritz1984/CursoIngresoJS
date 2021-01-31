839d3ab1ea8111fdfb9f6a9e74203ca21aa4a6bb
lfritz1984 <leonelfritz@gmail.com>
2021-01-31 15:21:56 -0300

    Ejercicio 4 terminado

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,14 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
-}
+	//reservo el espacio en memoria para guardar el nombre del usuario (variable)
+	let nombre; 
 
+    //guardo en la variable nombre el texto que escribo en usuario dentro de la entana prompt
+	nombre = prompt("Ingrese su nombre"); 
+
+    //copio en nombre que tengo guardado en la variable nombre dentro de la caja de texto embebida en la pagina html
+	document.getElementById("txtIdNombre").value = nombre; 
+
+	alert(nombre);
+}
