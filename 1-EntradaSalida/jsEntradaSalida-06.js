ea3c6aa1054318be9005cc649b9c9de0802e185f
lfritz1984 <leonelfritz@gmail.com>
2021-01-31 16:59:56 -0300

    Ejercicio 6 terminado

--- 1-EntradaSalida/jsEntradaSalida-06.js
+++ 1-EntradaSalida/jsEntradaSalida-06.js
@@ -4,6 +4,24 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
-	alert("ok");
-}
+	let num1;
+	let num2;
+	let resultado;
 
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	num2 = document.getElementById("txtIdNumeroDos").value;
+	
+	num1 = parseFloat(num1)
+	num2 = parseFloat(num2)
+	resultado = num1 + num2;
+
+	/* es otra manera de escribir el codigo y mismo resultado // y tambien hay dos comandos 
+	parseInt (numeros enteros)
+	parseFloat (numeros con decimales)
+	
+	num1 = parseFloat(document.getElementById("txtIdNumeroUno").value);
+	num2 = parseFloat(document.getElementById("txtIdNumeroDos").value) ;
+	resultado = num1 + num2;*/
+	
+	alert("La suma es " + resultado);
+}
