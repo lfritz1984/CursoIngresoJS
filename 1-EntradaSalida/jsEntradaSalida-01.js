c1f73d42919a87d1ca75b19efd10909fed167fc9
lfritz1984 <leonelfritz@gmail.com>
2021-01-31 11:29:11 -0300

    Ejercicio uno realizado

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,7 +3,7 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto funciona de Maravilla");
-	alert("Esto funciona de Maravilla 2");
-	alert("Ultima prueba");
+	alert("Esto funciona de maravilla");
+	alert("Esto funciona de maravilla 2");
+	alert("Esto funciona de maravilla 3");
 }
