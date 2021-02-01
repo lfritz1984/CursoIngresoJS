6e9e5ceb2ba17fce99347238b325ad574ee5eff5
lfritz1984 <leonelfritz@gmail.com>
2021-02-01 16:51:35 -0300

    Ejercicio 9 sin terminar

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -5,6 +5,11 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	let sueldo
+	let aumento
+	let resultado
 
+	sueldo = document.getElementById("txtIdSueldo").value;
+	aumento = 
 	alert("ok");
 }
