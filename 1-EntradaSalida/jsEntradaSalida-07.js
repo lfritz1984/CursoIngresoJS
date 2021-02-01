e9a9d329a75d08a8b5289a8b6efb33f372fade89
Leonel Fritz <lfritz@DASLOCAL.gob>
2021-02-01 15:00:57 -0300

    Ejercicio 7 terminado

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,69 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	let num1;
+	let num2;
+	let resultado
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	num2 = document.getElementById("txtIdNumeroDos").value;
+
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+
+	resultado = num1 + num2;
+
+	alert("La suma es " + resultado);	
 }
 
 function restar()
 {
-	
+	let num1;
+	let num2;
+	let resultado
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	num2 = document.getElementById("txtIdNumeroDos").value;
+
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+
+	resultado = num1 - num2;
+
+	alert("La resta es " + resultado);	
 }
 
 function multiplicar()
 { 
-	
+	let num1;
+	let num2;
+	let resultado
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	num2 = document.getElementById("txtIdNumeroDos").value;
+
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+
+	resultado = num1 * num2;
+
+	alert("La multiplicación es " + resultado);	
 }
 
 function dividir()
 {
-	
+	let num1;
+	let num2;
+	let resultado
+
+	num1 = document.getElementById("txtIdNumeroUno").value;
+	num2 = document.getElementById("txtIdNumeroDos").value;
+
+	num1 = parseFloat(num1);
+	num2 = parseFloat(num2);
+
+	resultado = num1 / num2;
+
+	alert("La división es " + resultado);
 }
 
