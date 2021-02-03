d0b82db8d52e76675d984bf009c2d2d4b6f9577f
Leonel Fritz <lfritz@DASLOCAL.gob>
2021-02-03 15:37:44 -0300

    TPs/03 realizado

--- 8-TPs/03-jsFerretePinturas.js
+++ 8-TPs/03-jsFerretePinturas.js
@@ -4,10 +4,32 @@ B.	Al ingresar una temperatura en Centígrados debemos mostrar la temperatura en
 */
 function FahrenheitCentigrados () 
 {
-	
+    let gFahr;
+    let Centígrados_1;
+
+    gFahr = document.getElementById("txtIdTemperatura").value;
+    gFahr = parseFloat(gFahr);
+
+    Centígrados_1 = (gFahr - 32) / 1.8;
+    Centígrados_1 = Centígrados_1.toFixed(2);
+
+    alert(gFahr + " grados Fahrenheit son " + Centígrados_1 + " grados Centígrados");
+
+    document.getElementById("txtIdTemperatura").value = "";
 }
 
 function CentigradosFahrenheit () 
 {
-	
+    let gCent;
+    let Fahrenheit_1;
+
+    gCent = document.getElementById("txtIdTemperatura").value;
+    gCent = parseFloat(gCent);
+
+    Fahrenheit_1 = (gCent * 1.8) + 32;
+    Fahrenheit_1 = Fahrenheit_1.toFixed(2);
+
+    alert(gCent + " grados Centígrados son " + Fahrenheit_1 + " grados Fahrenheit");
+    
+    document.getElementById("txtIdTemperatura").value = "";
 }
