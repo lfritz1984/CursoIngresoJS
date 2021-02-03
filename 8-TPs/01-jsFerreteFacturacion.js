0491e060a0dff9703f60c6a47e077e6eed0e19be
Leonel Fritz <lfritz@DASLOCAL.gob>
2021-02-03 11:29:57 -0300

    TPs/01 realizado

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,63 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    let precio01;
+    let precio02;
+    let precio03;
+    let PrecioFinal;
+
+    precio01 = document.getElementById("txtIdPrecioUno").value;
+    precio02 = document.getElementById("txtIdPrecioDos").value;
+    precio03 = document.getElementById("txtIdPrecioTres").value;
+
+    precio01 = parseFloat(precio01);
+    precio02 = parseFloat(precio02);
+    precio03 = parseFloat(precio03);
+
+    PrecioFinal = precio01 + precio02 + precio03;
+    PrecioFinal = PrecioFinal.toFixed(2);
+
+    alert("La suma de sus productos es: " + PrecioFinal);
 }
 function Promedio () 
 {
-	
+    let proDucto01;
+    let proDucto02;
+    let proDucto03;
+    let Promedio;
+
+    proDucto01 = document.getElementById("txtIdPrecioUno").value;
+    proDucto02 = document.getElementById("txtIdPrecioDos").value;
+    proDucto03 = document.getElementById("txtIdPrecioTres").value;
+
+    proDucto01 = parseFloat(proDucto01);
+    proDucto02 = parseFloat(proDucto02);
+    proDucto03 = parseFloat(proDucto03);
+
+    Promedio = (proDucto01 + proDucto02 + proDucto03) / 3;
+    Promedio = Promedio.toFixed(2);
+
+    alert("El promedio de sus productos es: " + Promedio);
 }
 function PrecioFinal () 
 {
-	
+    let sinIVA01;
+    let sinIVA02;
+    let sinIVA03;
+    let sumaparcial;
+    let precioFinal;
+
+    sinIVA01 = document.getElementById("txtIdPrecioUno").value;
+    sinIVA02 = document.getElementById("txtIdPrecioDos").value;
+    sinIVA03 = document.getElementById("txtIdPrecioTres").value;
+
+    sinIVA01 = parseFloat(sinIVA01);
+    sinIVA02 = parseFloat(sinIVA02);
+    sinIVA03 = parseFloat(sinIVA03);
+
+    sumaparcial = sinIVA01 + sinIVA02 + sinIVA03;
+    precioFinal = (sumaparcial * 21 / 100) + sumaparcial;
+    precioFinal = precioFinal.toFixed(2);
+
+    alert("Su precio consumidor final es: " + precioFinal);
 }
