908005da0af8c4106be03a1d531d60ce6abdc49e
lfritz1984 <leonelfritz@gmail.com>
2021-02-04 22:17:30 -0300

    Parciales E/S 2019 realizado

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
+    let precio;
+    let porcenDescuento;
+    let descuento;
+    let precioFinal;
 
+    precio = prompt("Ingrese el precio a descontar");
+    porcenDescuento = prompt("Ingrese el porcentaje de descuento que quiere");
+
+    precio = parseFloat(precio);
+    porcenDescuento = parseFloat(porcenDescuento);
+
+    descuento = (precio * porcenDescuento) / 100;
+    precioFinal = precio - descuento;
+
+    document.getElementById("elPrecioFinal").value = precioFinal;
 }
