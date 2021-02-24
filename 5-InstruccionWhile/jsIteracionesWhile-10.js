ea3d280af1bfb3035cfea3111f26cf70dafde93a
Leonel Fritz <lfritz@DASLOCAL.gob>
2021-02-24 17:15:22 -0300

    tratando de hacer parcial 2020 ejercicio 1

--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -13,25 +13,136 @@ hasta que el usuario quiera, mostrar:
 function mostrar()
 {
 	//declarar contadores y variables 
-	let respuesta;
-	let numeroIngresado;
-	let sumaNegativos = 0;
-	let sumaPositivos = 0;
-	let contadorDePositivos;
-	let contadorDeNegativos;
-	let contadorDeCeros;
-	let contadorDenumerosPares;
-	let promedioDePositivos;
-	let promedioDeNegativos;
 
-	respuesta="si";
+	var numeroIngresado;
+	var acumuladorNegativos;
+	var acumuladorPositivos;
+	var cantidadNegativos;
+	var cantidadPositivos;
+	var cantidadCeros;
+	var cantidadPares;
+	var promedioNegativos;
+	var promedioPositivos;
+	var diferenciaPositivosNegativos;
+	var maximoNegativo;
+	var minimoPar;
+	var respuesta;
+
+
+	respuesta = "si";
+	acumuladorNegativos = 0;
+	acumuladorPositivos = 0;
+	cantidadPositivos = 0;
+	cantidadNegativos = 0;
+	cantidadCeros = 0;
+	cantidadPares = 0;
+
 
 	while(respuesta=="si")
 	{
-		numeroIngresado = prompt("Ingrese un numero");
-		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+		numeroIngresado = prompt("ingrese un numero");
+		numeroIngresado = parseInt(numeroIngresado);
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+		while(isNaN(numeroIngresado)) //isNaN = no es un numero? //es por si NO es un numero
+		{
+			numeroIngresado = prompt("ERROR reingrese un numero");
+			numeroIngresado = parseInt(numeroIngresado);
+		}
+
+		console.log(numeroIngresado);
+
+		if (numeroIngresado < 0)
+		 {
+		 	if (cantidadNegativos == 0 || numeroIngresado > maximoNegativo)
+		 	{
+		 		maximoNegativo = numeroIngresado;
+		 	}
+
+		 	acumuladorNegativos = acumuladorNegativos + numeroIngresado;
+		 	cantidadNegativos ++
+		 }
+		 else
+		 {
+		 	if (numeroIngresado > 0)
+		 	 {
+		 	 	acumuladorPositivos = acumuladorPositivos + numeroIngresado;
+		 		cantidadPositivos ++ //canitdadDepositivos = cantidadDePositivos + 1
+		 	 }
+			else 
+			{
+				cantidadCeros ++;
+			}		 
+		 }
+
+		 if (numeroIngresado % 2 == 0) //para numeros pares
+		 {
+		 
+		 	/*if (cantidadPares == 0) //es el primer par
+		 	{
+		 		minimoPar = numeroIngresado;
+		 	}
+		 	else
+		 	{
+		 		if (numeroIngresado minimoPar)
+		 		 {
+		 		 	minimoPar = numeroIngresado
+		 		 }	*/
+		 		
+		 		 if (cantidadPares == 0 || numeroIngresado < minimoPar) //diferencia entre pares
+		 		  {
+		 		  	minimoPar = numeroIngresado;
+		 		  }
+
+		 	cantidadPares ++;
+		 }
+
+		respuesta=prompt("desea continuar? si/no");
+	}
+	
+	diferenciaPositivosNegativos = acumuladorPositivos - acumuladorNegativos //9-Diferencia entre positivos y negativos, (positvos-negativos).
+
+
+	document.write("la suma de los negativos es :" + acumuladorNegativos);
+	document.write("la suma de los positivos es :" + acumuladorNegativos);
+	
+	document.write("la cantidad de los negativos es :" + cantidadNegativos);
+	document.write("la cantidad de los positivos es :" + cantidadPositivos;
+
+	document.write("la cantidad de ceros es :" + cantidadCeros);
+
+	document.write("la cantidad de pares es :" + cantidadPares);
+
+	if (cantidadDePositivos != 0)
+	 {
+		 promedioPositivos = acumuladorPositivos / cantidadPositivos;
+		 document.write("el promedio de los positivos es :" + promedioPositivos);
+	 }
+		else
+		{
+			document.write("el promedio de los positivos es: no hubo ingreso de positivos" );
+		}
+	
+	if (cantidadNegativos != 0)
+ 	{
+		promedioNegativos = acumuladorNegativos / cantidadNegativos;
+    	document.write("el promedio de los negativos es :" + promedioNegativos);
+ 	}
+ 		else
+ 		{
+ 			document.write("el promedio de los negativos es: no hubo ingreso de negativos");
+		}
+
+		document.write("la diferencia entre positivos y negativos es :" + diferenciaPositivosNegativos);
 }//FIN DE LA FUNCIÓN
+
+/*
+	contadores: 
+	contador = contador + 1/2/3;
+	contador ++;(incrementa de uno en uno)(igual al de arriba)
+
+	acumuladores:
+	acumulador = acumulador + valor;
+
+	acumulador + = valor; (es lo mismo que lo de arriba)
+
+*/
--- 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/uno.js
@@ -1,5 +1,85 @@
+/*
+Enunciado:
 
+Debemos realizar la carga de 5(cinco) productos de prevención de contagio,
+de cada una debo obtener los siguientes datos:
+el tipo (validar "barbijo" , "jabón" o "alcohol") ,
+el precio (validar entre 100 y 300),
+la cantidad de unidades (no puede ser 0 o negativo y no debe superar las 1000 unidades),
+la Marca y el fabricante.
+Se debe Informar al usuario lo siguiente:
+a) Del más barato de los alcohol, la cantidad de unidades y el fabricante
+b) Del tipo con mas unidades, el promedio por compra
+c) Cuántas unidades de jabones hay en total
+*/
 function mostrar()
 {
-	alert("uno");
+	let tipoDeProducto;
+	let precioIngresado;
+	let cantidadDeUnidades;
+	let marcaDelProducto;
+	let fabricanteDelProducto;
+	let alcoholMasBarato;
+	let fabricanteAlcoholMasBarato;
+
+	let contadorDeProductos = 0;
+
+	let contadorDeAlcohol = 0;
+	let contadorDeJabon = 0
+	let contadorDeBarbijos = 0;
+
+	let acumuladorDeAlcohol;
+	let acumuladorDejabon;
+	let acumuladorDeBarbijos;
+
+	let stockDeAlcohol = 0;
+	let stockDeJabon = 0;
+	let StockDEBarbijos = 0;
+
+	while(contadorDeProductos < 5)
+	{
+		tipoDeProducto = prompt("Ingrese un tipo de producto: barbijo, jabon o alcohol");
+		tipoDeProducto = tipoDeProducto.toLowerCase();
+        while(isNaN(tipoDeProducto) == false || (tipoDeProducto != "barbijo" && tipoDeProducto != "jabon" && tipoDeProducto != "alcohol"))
+		{
+			tipoDeProducto = prompt("Reingrese un producto valido");
+		}
+
+		precioIngresado = prompt("Ingrese el precio del producto");
+		while(isNaN(precioIngresado) == true || (precioIngresado < 100 || precioIngresado > 300))
+		{
+			precioIngresado = prompt("Reingrese, el precio");
+		}
+
+		cantidadDeUnidades = prompt("Ingrese la cantidad de productos");
+		while(isNaN(cantidadDeUnidades) == true || cantidadDeUnidades <= 0 || cantidadDeUnidades > 1000)
+		{
+			cantidadDeUnidades = prompt("Reingrese, cantidad de productos");
+		}
+
+		marcaDelProducto = prompt("Ingrese la Marca del preducto");
+		fabricanteDelProducto = prompt("Ingrese el fabricante del producto");
+
+		switch(tipoDeProducto)
+		{
+			case "barbijo":
+				contadorDeBarbijos ++
+				acumuladorDeBarbijos = acumuladorDeBarbijos + precioIngresado;
+				break;
+			case "jabon":
+				contadorDeJabon ++
+				acumuladorDejabon = acumuladorDejabon + precioIngresado;
+				break;
+			case "alcohol":
+				contadorDeAlcohol ++
+				acumuladorDeAlcohol = acumuladorDeAlcohol + precioIngresado;
+				break;	
+		}
+		contadorDeProductos	++	
+	}
+	if(tipoDeProducto == "alcohol" )
+	    {
+			alert(acumuladorDeAlcohol);
+	    }
+	
 }
