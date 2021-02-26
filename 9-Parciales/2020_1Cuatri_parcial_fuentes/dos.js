4b22871ca4c6a6b90d7d2e2cbc7eb67ed4151222
Leonel Fritz <lfritz@DASLOCAL.gob>
2021-02-26 16:55:02 -0300

    Ejercicio 2 parcial 2020 sin terminar

--- 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
+++ 9-Parciales/2020_1Cuatri_parcial_fuentes/dos.js
@@ -1,16 +1,3 @@
-/*
-Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, hasta que el cliente quiera:
-Tipo validad("arena";"cal";"cemento")
-Cantidad de bolsas,
-Precio por bolsa (más de cero ),
-
-Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar.
-Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar.
-a) El importe total a pagar , bruto sin descuento y...
-b) el importe total a pagar con descuento(solo si corresponde)
-d) Informar el tipo con mas cantidad de bolsas.
-f) El tipo mas caro
-*/
 function mostrar()
 {
   let producto;
@@ -18,35 +5,31 @@ function mostrar()
   let precioPorBolsa;
   let precioBrutoTotal;
   let precioFinalconDescuento;
-  let descuento;
   let respuesta;
 
-  let acumuladorBolsasArena;
+  let acumuladorBolsasDeArena;
   let acumuladorBolsasCal;
   let acumuladorBolsasCemento;
   let acumuladorTotaldeBolsas;
   let precioBrutoArena;
   let precioBrutoCal;
   let precioBrutoCemento;
-
-  let mayorCantidadDeBolsas;
+  
   let tipoConMasCantidadDeBolsas;
+  let mayorCantidadDeBolsas;
   let tipoMasCaro;
+  let bolsaMasCara;
 
-  respuesta = "si";
-  banderaArena = true;
-  banderaCal = true;
-  banderaCemento= true;
- 
-
-  acumuladorBolsasArena = 0;
+  acumuladorBolsasDeArena = 0;
   acumuladorBolsasCal = 0;
   acumuladorBolsasCemento = 0;
   acumuladorTotaldeBolsas = 0;
   precioBrutoArena = 0;
   precioBrutoCal = 0;
   precioBrutoCemento = 0;
-  tipoConMasCantidadDeBolsas = 0;
+  bolsaMasCara = 0;
+  mayorCantidadDeBolsas = 0;
+  respuesta = "si";
 
   while(respuesta == "si")
   {
@@ -73,11 +56,11 @@ function mostrar()
     switch(producto)
     {
       case "arena":
-        acumuladorBolsasDeArena = acumuladorBolsasArena + cantidadDeBolsas;
-        precioBrutoArena = acumuladorBolsasArena * precioPorBolsa;
+        acumuladorBolsasDeArena = acumuladorBolsasDeArena + cantidadDeBolsas;
+        precioBrutoArena = acumuladorBolsasDeArena * precioPorBolsa;
         break;
       case "cal":
-        acumuladorDeCal = acumuladorDeCal + cantidadDeBolsas;
+        acumuladorBolsasCal = acumuladorBolsasCal + cantidadDeBolsas;
         precioBrutoCal = acumuladorBolsasCal * precioPorBolsa;
         break;
        case "cemento":
@@ -85,39 +68,129 @@ function mostrar()
          precioBrutoCemento = acumuladorBolsasCemento * precioPorBolsa;
          break;
     }
-    if(tipoConMasCantidadDeBolsas < cantidadDeBolsas)
+    if(mayorCantidadDeBolsas < cantidadDeBolsas)
     {
-      mayorCantidadDeBolsas = cantidadDeBolsas
+      mayorCantidadDeBolsas = cantidadDeBolsas;
+      tipoConMasCantidadDeBolsas = producto;
     }
-
+    if(bolsaMasCara < precioPorBolsa)
+    {
+      bolsaMasCara = precioPorBolsa;
+      tipoMasCaro = producto;
+    }
     respuesta = prompt("¿Desea seguir ingresando productos?, responda con: si ó no");
     respuesta = respuesta.toLocaleLowerCase();
   }
-  
-  acumuladorTotaldeBolsas = acumuladorBolsasDeArena + acumuladorBolsasArena + acumuladorBolsasCemento;
-
-  //a) El importe total a pagar , bruto sin descuento y...
-   precioBrutoTotal = acumuladorBolsasArena + acumuladorBolsasCal + acumuladorBolsasCemento;
-
-  //b) el importe total a pagar con descuento(solo si corresponde)
+  acumuladorTotaldeBolsas = acumuladorBolsasDeArena + acumuladorBolsasCal + acumuladorBolsasCemento;
+  precioBrutoTotal = precioBrutoArena + precioBrutoCal + precioBrutoCemento;
+  document.write("Su total en bruto es: " + precioBrutoTotal + "<br>");
   if(acumuladorTotaldeBolsas > 10 && acumuladorTotaldeBolsas < 31)
   {
-    descuento = 15;
+    precioFinalconDescuento = precioBrutoTotal * 0.85;
+    document.write("El importe a pagar con descuento es: " + precioFinalconDescuento + "<br>");
   }
   else
   {
     if(acumuladorTotaldeBolsas > 30)
     {
-      descuento = 25;
+      precioFinalconDescuento = precioBrutoTotal * 0.75;
+      document.write("El importe a pagar con descuento es: " + precioFinalconDescuento + "<br>");
     }
   }
-   precioFinalconDescuento = precioFinalconDescuento - (precioBrutoTotal * descuento / 100);
-
-   //d) Informar el tipo con mas cantidad de bolsas.
-
-   
-
-   //f) El tipo mas caro
-
+  document.write("El tipo con mas cantidad de bolsas es: " + tipoConMasCantidadDeBolsas + "<br>");
+  document.write("El tipo mas caro es: " + tipoMasCaro + "<br>");
 }
-// https://www.onlinegdb.com/rkePvnHMu
+
+
+
+
+/*
+/* MAGALI GARCIA
+ Realizar el algoritmo que permita ingresar los datos de una compra productos de la construccion, hasta que el cliente quiera:
+Tipo validad("arena";"cal";"cemento")
+Cantidad de bolsas,
+Precio por bolsa (más de cero ),
+
+X) Si compro más de 10 bolsas en total tenes 15% de descuento sobre el total a pagar. 
+X) Si compro más de 30 bolsas en total tenes 25% de descuento sobre el total a pagar. 
+X)  a) El importe total a pagar , bruto sin descuento y... 
+X)  b) el importe total a pagar con descuento(solo si corresponde)
+X)  d) Informar el tipo con mas cantidad de bolsas.
+X)  f) El tipo mas caro
+
+function mostrar()
+{
+    var ingresarOtroProducto;
+    var tipoProducto;
+    var cantidadDeBolsas;
+    var acumuladorTotalDeBolsas;
+    var acumuladorAPagarBruto;
+    var precioPorBolsa;
+    var totalAPagarBruto;
+    var totalAPagarConDescuento;
+    var mayorCantidadDeBolsas;
+    var tipoMayorCantidadDeBolsas;
+    var tipoMasCaro;
+    var precioDelMasCaro;
+
+
+    acumuladorTotalDeBolsas = 0;
+    acumuladorAPagarBruto = 0;
+    precioDelMasCaro = 0;
+    tipoMayorCantidadDeBolsas = 0;
+
+    ingresarOtroProducto = "si";
+    while (ingresarOtroProducto == "si") { //INGRESA DATOS HASTA QUE QUIERA
+        tipoProducto = prompt("Ingrese el tipo del producto: ARENA, CAL O CEMENTO:").toLowerCase();
+        while (tipoProducto != "arena" && tipoProducto != "cal" && tipoProducto != "cemento") {
+            tipoProducto = prompt("ERROR!. El tipo del producto debe ser: ARENA, CAL O CEMENTO:").toLowerCase();
+        }
+
+        cantidadDeBolsas = prompt("Ingrese la cantidad de bolsas");
+        cantidadDeBolsas = parseInt(cantidadDeBolsas);
+        while (isNaN(cantidadDeBolsas) == true) {
+            cantidadDeBolsas = prompt("ERROR! Ingrese en números la cantidad de bolsas que desea: ");
+        }
+        
+        acumuladorTotalDeBolsas = acumuladorTotalDeBolsas + cantidadDeBolsas;
+
+        precioPorBolsa = prompt("Ingrese el precio por bolsas");
+        precioPorBolsa = parseInt(precioPorBolsa);
+        while (isNaN(precioPorBolsa) == true || precioPorBolsa < 0) {
+            precioPorBolsa = prompt("ERROR! Ingrese en números el precio por bolsa: ");
+        }
+            
+        totalAPagarBruto = precioPorBolsa * cantidadDeBolsas; //TOTAL BRUTO POR PRODUCTO
+        acumuladorAPagarBruto = acumuladorAPagarBruto + totalAPagarBruto; //TOTAL BRUTO POR COMPRA
+         //tipo con mas cantidad de bolsas
+        if(tipoMayorCantidadDeBolsas < cantidadDeBolsas){
+            mayorCantidadDeBolsas = cantidadDeBolsas; 
+            tipoMayorCantidadDeBolsas = tipoProducto;
+        }
+        //El tipo mas caro
+        if(precioPorBolsa > precioDelMasCaro){
+            precioDelMasCaro = precioPorBolsa;
+            tipoMasCaro = tipoProducto;
+        }
+        //MODIFICO VARIABLE DE CONTROL
+        ingresarOtroProducto = prompt("Desea comprar otro producto? Responda Si o No: ").toLowerCase();
+        while (ingresarOtroProducto != "si" && ingresarOtroProducto != "no") {
+            ingresarOtroProducto = prompt("ERROR! Responda Si o No: ").toLowerCase();
+        }
+    }//FIN WHILE INGRESO DE PRODUCTOS
+
+    if(acumuladorTotalDeBolsas > 30){   // DESCUENTO SI SON MAS DE 30 BOLSAS
+            totalAPagarConDescuento = totalAPagarBruto * 25 / 100;
+            document.write("El importe con decuento es: "+totalAPagarConDescuento+"<br>");
+    } else {
+        if(acumuladorTotalDeBolsas > 10 && acumuladorTotalDeBolsas < 31){ // SI SON  MAS DE 10 PERO MENOS DE 30
+        totalAPagarConDescuento = totalAPagarBruto * 15 / 100;
+        document.write("El importe con decuento es: "+totalAPagarConDescuento+"<br>");
+        }else{
+            document.write("El importe total es: "+totalAPagarBruto+ "<br>"); // MENOS DE 10 BOLSAS
+        }
+    }
+    document.write("El tipo con mas cantidad de bolsas es: "+tipoMayorCantidadDeBolsas+"<br>");
+    document.write("El tipo más caro es: "+tipoMasCaro);
+}// FIN DE LA FUNCION
+*/
